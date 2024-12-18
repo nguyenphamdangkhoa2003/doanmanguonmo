d711e4d3c71b1a1ab2052890a05a27465c066f3b
Duong Nhat Thien <dangkhoa2k30@gmail.com>
2024-12-18 18:09:27 +0700

    Thêm chức năng thêm phòng

--- routes/auth.php
+++ routes/auth.php
@@ -54,4 +54,5 @@ Route::middleware("auth")->group(function () {
 Route::middleware(["auth", IsAdminMiddleware::class])->group(function () {
     Volt::route("admin/list-user", ListUser::class)->name("list-user");
     Volt::route("admin/list-room", ListRoom::class)->name("list-room");
+    Volt::route("admin/add-room", AddRoom::class)->name("add-room");
 });

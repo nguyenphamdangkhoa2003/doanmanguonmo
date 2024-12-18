3220a1cd1fb0340ef6557efdd338c71e66b90a32
Duong Nhat Thien <dangkhoa2k30@gmail.com>
2024-12-18 17:38:30 +0700

    Lấy dữ liệu phòng từ database

---
+++ app/Livewire/Pages/Admin/ListRoom.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire\Pages\Admin;
+
+use App\Models\Room;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+use Livewire\WithPagination;
+use Mary\Traits\Toast;
+
+class ListRoom extends Component
+{
+    use Toast;
+    use WithPagination;
+    public $headers = [
+        ['key' => 'id', 'label' => '#'],
+        ['key' => 'room_number', 'label' => 'Room Number'],
+        ["key" => "room_type.room_type_name", "label" => "Room Type"],
+        ["key" => "action", "label" => "Action", 'sortable' => false]
+    ];
+    #[Layout("components.layouts.admin")]
+    public array $sortBy = ['column' => 'room_number', 'direction' => 'asc'];
+    public string $search = "";
+    public function render()
+    {
+        $rooms = Room::all();
+
+        return view('livewire.pages.admin.list-room', [
+            "rooms" => $rooms,
+        ]);
+    }
+}

2572229b2a8911deadb11c6384ee8d48c4b6ade2
quikiet <quikiet03@gmail.com>
2024-12-19 21:00:06 +0700

    Thêm giao diện thống kê theo số lượng đơn đặt phòng

--- resources/views/livewire/pages/admin/dashboard.blade.php
+++ resources/views/livewire/pages/admin/dashboard.blade.php
@@ -16,7 +16,22 @@
                 </div>
             </x-mary-card>
         </a>
-
+        <a href="{{route('list-booking')}}" class="link no-underline hover:shadow-lg">
+            <x-mary-card class="bg-white rounded-none shadow border h-32" separator progress-indicator>
+                <div class="flex p-5 gap-3">
+                    <x-mary-icon name="s-clipboard-document-check"
+                        class="w-12 h-12 bg-slate-200 text-blue-700 p-2 rounded-full" />
+                    <div>
+                        <div class="font-extrabold pb-1">
+                            {{$bookingCount}}
+                        </div>
+                        <div class="text-slate-300 font-extrabold text-sm">
+                            Bookings
+                        </div>
+                    </div>
+                </div>
+            </x-mary-card>
+        </a>
     </div>
 
 

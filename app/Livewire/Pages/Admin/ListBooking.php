5904ccd97810d94432c9aba27d0447da35d6d05d
Duong Nhat Thien <dangkhoa2k30@gmail.com>
2024-12-19 20:02:14 +0700

    Thêm chức năng cập nhật trạng thái đơn đặt phòng

--- app/Livewire/Pages/Admin/ListBooking.php
+++ app/Livewire/Pages/Admin/ListBooking.php
@@ -32,4 +32,32 @@ class ListBooking extends Component
             "bookings" => $bookings
         ]);
     }
+
+    public function approve($id)
+    {
+        $booking = Booking::find($id);
+        if (!$booking) {
+            return $this->error("Not found");
+        }
+        $booking->status = "confirm";
+        $booking->save();
+        return $this->success("Approve successfully");
+
+    }
+
+    public function delete($id)
+    {
+        $booking = Booking::find($id);
+        if (!$booking) {
+            return $this->error("Not found");
+        }
+        $booking->status = "cancel";
+        $booking->save();
+        return $this->success("Cancel successfully");
+    }
+
+    public function detail($id)
+    {
+        return $this->redirectRoute("detail-booking", ["id" => $id]);
+    }
 }

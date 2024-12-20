9fa6e85ae9156c20d35f92572c42fd7065c49101
Duong Nhat Thien <dangkhoa2k30@gmail.com>
2024-12-20 16:46:04 +0700

    Lây danh sách tin nhắn liên hệ trong trang Admin

---
+++ app/Livewire/Pages/Admin/ListContactMessage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire\Pages\Admin;
+
+use App\Models\ContactMessage;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+#[Layout("components.layouts.admin")]
+class ListContactMessage extends Component
+{
+    public function render()
+    {
+        $messages = ContactMessage::orderBy('created_at', 'desc')->get();
+        return view('livewire.pages.admin.list-contact-message', [
+            "messages" => $messages,
+        ]);
+    }
+}

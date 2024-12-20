ca99a5563dd1939c1b77b05e5259f66cd1f7bcc7
quikiet <quikiet03@gmail.com>
2024-12-20 15:49:04 +0700

    Thêm giao diện quản lí Banner

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -79,7 +79,10 @@
                 <x-mary-menu-item title="Rooms" icon="o-home-modern" link="{{ route('list-room') }}" />
                 <x-mary-menu-item title="Room Types" icon="o-tag" link="{{ route('list-type-room') }}" />
                 <x-mary-menu-item title="Booking" icon="o-shopping-cart" link="{{ route('list-booking') }}" />
+                <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
+                    <x-mary-menu-item title="Banners" icon="o-banknotes" link="{{ route('banners') }}" />
 
+                </x-mary-menu-sub>
             </x-mary-menu>
         </x-slot:sidebar>
 
--- resources/views/livewire/pages/admin/banners.blade.php
+++ resources/views/livewire/pages/admin/banners.blade.php
@@ -1,3 +1,41 @@
 <div>
-    
+    <x-mary-header title="Banners" separator progress-indicator />
+    <div>
+        <x-mary-file wire:model="photos" multiple />
+    </div>
+    <div class="flex gap-3 flex-wrap my-3">
+        @foreach ($photos as $key => $photo)
+            <div class="card bg-base-100 image-full w-96 shadow-xl">
+                <figure>
+                    <img src="{{ $photo->temporaryUrl() }}" alt="Preview" />
+                </figure>
+                <div class="card-body">
+                    <div class="card-actions justify-end">
+                        <x-mary-button icon="o-trash" wire:click.prevent="deleteSelected({{ $key }})" spinner
+                            class="btn-sm text-red-400"
+                            wire:confirm.prompt="Are you sure?\n\nType DELETE to confirm|DELETE" />
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <x-mary-button label="Save" icon-right="o-paper-airplane" class="btn-primary" spinner wire:click="save" />
+    <div class="flex gap-3 flex-wrap my-3">
+        @if (isset($images))
+            @foreach ($images as $image)
+                <div class="card bg-base-100 image-full w-96 shadow-xl">
+                    <figure>
+                        <img src="{{ $image->url }}" alt="Preview" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="card-actions justify-end">
+                            <x-mary-button icon="o-trash" wire:click.prevent="delete({{ $image->id }})" spinner
+                                class="btn-sm text-red-400"
+                                wire:confirm.prompt="Are you sure?\n\nType DELETE to confirm|DELETE" />
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @endif
+    </div>
 </div>

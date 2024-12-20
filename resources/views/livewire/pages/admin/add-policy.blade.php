fb3794043e37262c07ee24ecc44aadb760be9945
quikiet <quikiet03@gmail.com>
2024-12-20 17:44:57 +0700

    Thêm giao diện quản lý thêm chính sách

--- resources/views/livewire/pages/admin/add-policy.blade.php
+++ resources/views/livewire/pages/admin/add-policy.blade.php
@@ -1,3 +1,11 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <x-mary-header title="Add policy" separator />
+    <x-mary-form wire:submit="save" no-separator>
+        <x-mary-input label="Polycy type " wire:model="form.policy_type" />
+        <x-mary-textarea label="Description" wire:model="form.description" />
+        <x-slot:actions>
+            <x-mary-button label="Cancel" />
+            <x-mary-button label="Save" class="btn-primary" type="submit" spinner="save" />
+        </x-slot:actions>
+    </x-mary-form>
 </div>
